6df2e719d76164c883e7859218cf166f4d043609
nico <nicolastanco001@gmail.com>
2021-04-11 20:45:15 -0300

    a

--- case7ejercicioExtra/clase7playground.js
+++ case7ejercicioExtra/clase7playground.js
@@ -1,14 +1,15 @@
-let array=["Fizz","Buzz"]
+let array=[ "Fizz","Buzz"]
 
 function FizzBuzz(x,y,arr){
     for(let i=1;i<=100;i++){ 
-    let k=0
-      if (x%i==0 && y%i==0){ k=arr[0]+arr[1]
-       } else if(x%i==0) {k=arr[0]
-       } else if(y%i==0) {
+    let k=i
+      if (i%x==0 && i%y==0){ k=arr[0]+arr[1]
+       } else if(i%x==0) {k=arr[0]
+       } else if(i%y==0) {
        k=arr[1]}
        console.log(k)
     }
 }
 
-FizzBuzz(80,90,array)
+
+
